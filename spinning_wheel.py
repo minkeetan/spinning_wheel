1b7ba6a4332486fa03a784906a020a904aa74c83
minkeetan <tminkee@gmail.com>
2021-07-18 22:15:09 +0800

    Reset time counter after 1 spin

--- spinning_wheel.py
+++ spinning_wheel.py
@@ -13,7 +13,7 @@ FPS = 60
 
 # RGB in a tuple
 WHITE = (255, 255, 255)
-GREY = (150, 150, 150)
+GREEN = (0, 200, 0)
 BLUE = (0, 0 , 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
@@ -21,6 +21,7 @@ YELLOW = (255, 255, 0)
 ORANGE = (255, 102, 0)
 
 NAME_FONT = pygame.font.SysFont('comicsans', 40)
+CLASS_FONT = pygame.font.SysFont('comicsans', 40)
 GAME_FONT = pygame.font.SysFont('comicsans', 80)
 TIPS_FONT = pygame.font.SysFont('comicsans', 25)
 TIME_FONT = pygame.font.SysFont('comicsans', 80)
@@ -28,7 +29,7 @@ TIME_FONT = pygame.font.SysFont('comicsans', 80)
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Pick me please")
 
-def draw_window(person, time_counter, start_spinning):
+def draw_window(person, time_counter, start_spinning, class_selected):
 
     # order of drawing does matter, as the screen that drew later will cover up the screen earlier
     WIN.fill(BLACK)
@@ -36,13 +37,15 @@ def draw_window(person, time_counter, start_spinning):
     game_title = GAME_FONT.render("Pick me please!", 1, BLACK)
     text_background = pygame.Rect(
         WIDTH // 2 - game_title.get_width() // 2, 50, game_title.get_width(), game_title.get_height())
+    
+    class_title = CLASS_FONT.render(class_selected, 1, GREEN)
+    WIN.blit(class_title, (10, 10))
 
     pygame.draw.rect(WIN, YELLOW, text_background)
     WIN.blit(game_title, (WIDTH // 2 - game_title.get_width() // 2, 50))
 
     name_bg = pygame.Rect(WIDTH // 2 - BG_WIDTH // 2, HEIGHT // 4 * 3, BG_WIDTH, BG_HEIGHT)
     # pygame.draw.rect(WIN, YELLOW, name_bg)
-
     if start_spinning == False or time_counter == 0:
         tips = TIPS_FONT.render("Press Space bar to start", 1, YELLOW)
     else:
@@ -52,9 +55,9 @@ def draw_window(person, time_counter, start_spinning):
 
     if person != "" and time_counter >= 0:
         if time_counter > 0:
-            timer = TIME_FONT.render(str(time_counter), 1, BLUE)
+            timer = TIME_FONT.render(str(time_counter), 1, GREEN)
         else:
-            timer = TIME_FONT.render("Congratulations!", 1, BLUE)
+            timer = TIME_FONT.render("Congratulations!", 1, GREEN)
 
         WIN.blit(timer, (WIDTH // 2 - timer.get_width() // 2, HEIGHT // 2 - 50))
         
@@ -81,6 +84,8 @@ def main():
 
     # load_name_list
     name_list = []
+    class_list = ["2ST1", "2SK1", "2TA1", "2TB1", "2TC1"]
+    class_index = 0
     person = ""
     time_counter = 5
 
@@ -96,22 +101,30 @@ def main():
             if event.type == ONE_SEC_TIMER and time_counter > 0 and start_spinning == True:
                 print("One sec timer tick!")
                 time_counter -= 1
+            
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP and class_index + 1 < len(class_list) and start_spinning == False:
+                    class_index += 1
+                elif event.key == pygame.K_DOWN and class_index - 1 >= 0 and start_spinning == False:
+                    class_index -= 1
 
+        class_selected = class_list[class_index]
         key_pressed = pygame.key.get_pressed()
         if key_pressed[pygame.K_SPACE] and start_spinning == False:
             start_spinning = True
+            time_counter = 5
             name_list = pd.read_csv("name_list.csv")
-            name_list = name_list[name_list["class"] == "2ST1"]["name"].to_list()
+            name_list = name_list[name_list["class"] == class_selected]["name"].to_list()
             random.shuffle(name_list)
             print(name_list)
-        elif key_pressed[pygame.K_SPACE] and start_spinning == True and time_counter == 0:
-            start_spinning = False
-            break
         
         if len(name_list) > time_counter:
             person = name_list[time_counter].strip()
+
+        if time_counter == 0:
+            start_spinning = False
             
-        draw_window(person, time_counter, start_spinning)
+        draw_window(person, time_counter, start_spinning, class_selected)
 
     main()
 
